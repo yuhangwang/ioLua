9e6ee95261cc86413a4e3ab38652d11c992eac40
Yuhang(Steven) Wang <stevenaura@live.com>
2016-10-29 17:33:06 -0500

    update build.bat to copy file to umolflowFramework

--- src/build.bat
+++ src/build.bat
@@ -2,3 +2,4 @@ del amalg.cache
 lua -lamalg ioLua.lua
 lua amalg.lua -s ioLua.lua -c -o ..\release\ioLua.lua
 copy ..\release\ioLua.lua ..\..\luapower
+copy ..\release\ioLua.lua ..\..\umolflowFramework\src
